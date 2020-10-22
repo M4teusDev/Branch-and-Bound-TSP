972192ca564abb03c8c3fd163acb3452dd8a19e6
Mateus Prado <mateus.silvaprado@gmail.com>
2020-10-21 22:42:13 -0300

    backup_v2
    Versão com struct "cidade" recebendo os dados do txt

--- BrancAndBound.c
+++ BrancAndBound.c
@@ -2,41 +2,55 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct city
-{
-    float cod;
-    float cord_x;
-    float cord_y;
-}CITY;
 
-int *recebe_matriz(char *path, CITY *p_city);
-int  tamanho_arquivo(char *path, char *line, FILE *p);
+    typedef struct cidade
+    {
+        float cod, cord_x, cord_y;
+    }cidade;
+
+
+//Receber dados
+int   recebe_dados(char *path, cidade **p_dados);
+int   tamanho_arquivo(char *path, char *line, FILE *p);
+int   tamanho_cabecalho( char *line, FILE *p);
+void  aloca(cidade **p, int tam);
+
+//Monta matriz
+void  monta_matriz(cidade *p, int lin, int col, float m[lin][col]);
 
 int main ( void )
 {
-    CITY *p_city;
+    cidade *p_cidade = NULL;
 
     char file[100] = "teste01.txt";
-    recebe_matriz(file, p_city);
+    int n = recebe_dados(file, &p_cidade);
+
+    float m[n][n];
+    monta_matriz(p_cidade, n, n, m);
 }
 
 
-int *recebe_matriz(char *path, CITY *p_city)
+int  recebe_dados(char *path, cidade **p_dados)
 {
     char line[1000];
-    FILE *p = NULL;
-    float x,y,z; //Variaveis auxiliares
+    FILE *p;
+    float x = 0, cod  = 0, y = 0; //Variaveis auxiliares
 
-    int tam = tamanho_arquivo(path, line, p) - 1, i = 0, head = 0;
+    int tam = tamanho_arquivo(path, line, p) - 1, i = 0, head = 0, cont = 0;
 
     if( (p = fopen(path,"r")) == NULL)  return 0; 
-    head = tamanho_cabecalho(path, line, p);
+    head = tamanho_cabecalho(line, p);
 
     for(i = 0; i < (tam - head) ; i++)
-    [
-        fscanf(p,"%f %f %f",&x, &y, &z);
-    ]
-        
+    {
+        fscanf(p,"%f %f %f",&cod, &x, &y);
+        aloca(p_dados, 3);
+
+        (*p_dados + cont)->cod = cod;
+        (*p_dados + cont)->cord_x = x;
+        (*p_dados + cont++)->cord_y = y;
+    }
+    return cont;        
 }
 
 int tamanho_arquivo(char *path, char *line, FILE *p)
@@ -56,7 +70,7 @@ int tamanho_arquivo(char *path, char *line, FILE *p)
     return tam;
 }
 
-int tamanho_cabecalho(char *path, char *line, FILE *p)
+int tamanho_cabecalho(char *line, FILE *p)
 {
     int k = 0;
 
@@ -67,4 +81,22 @@ int tamanho_cabecalho(char *path, char *line, FILE *p)
     }while( !feof(p) );
 
     return k;
+}
+
+void aloca(cidade **p, int tam){
+
+  if( (*p = (cidade *)realloc( *p ,tam * sizeof(cidade))) == NULL) {
+        printf("Estamos sem memoria...");
+        exit(1);
+    }
+}
+
+void  monta_matriz(cidade *p, int lin, int col, float m[lin][col])
+{
+    int i = 0, j = 0;
+
+    for(i = 0; i < lin; i++)
+    {
+         printf("%f\n", (p + i)->cod);    
+    }
 }
